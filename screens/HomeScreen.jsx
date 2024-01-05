1c41316851a4a76de96070b40eb1e2938deb8451
nblack0917 <nickablack@gmail.com>
2024-01-05 16:40:32 -0600

    added google places search

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,9 +1,16 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { setDestination, setOrigin } from "../slices/navSlice";
+
 import NavOptions from "../components/NavOptions";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView>
       <View className="p-5">
@@ -18,6 +25,38 @@ const HomeScreen = () => {
           }}
         />
 
+        <GooglePlacesAutocomplete
+          placeholder="From Where?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          minLength={2}
+          enablePoweredByContainer={false}
+          returnKeyType={"search"}
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            // console.log(data, details);
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+        />
         <NavOptions />
       </View>
     </SafeAreaView>
