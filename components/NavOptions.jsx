d482903a8e5ec50d1b4f60a74daf85f8ba42abba
nblack0917 <nickablack@gmail.com>
2024-01-09 11:07:02 -0600

    Added Google Autocomplete and maps

--- components/NavOptions.jsx
+++ components/NavOptions.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { Icon } from "@rneui/themed";
 import tw from "twrnc";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slices/navSlice";
 
 const data = [
   {
@@ -21,6 +23,7 @@ const data = [
 
 const NavOptions = () => {
   const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
 
   return (
     <FlatList
@@ -31,8 +34,9 @@ const NavOptions = () => {
         <TouchableOpacity
           onPress={() => navigation.navigate(item.screen)}
           className="p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40"
+          disabled={!origin}
         >
-          <View>
+          <View className={!origin && "opacity-20"}>
             <Image
               source={{
                 uri: item.image,
--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,12 +1,33 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import Map from "../components/Map";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import NavigateCard from "../components/NavigateCard";
+import RideOptionsCard from "../components/RideOptionsCard";
 
 const MapScreen = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <SafeAreaView>
-      <Text>MapScreen</Text>
-    </SafeAreaView>
+    <View>
+      <View className="h-1/2">
+        <Map />
+      </View>
+      <View className="h-1/2">
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="RideOptionsCard"
+            component={RideOptionsCard}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </View>
+    </View>
   );
 };
 
