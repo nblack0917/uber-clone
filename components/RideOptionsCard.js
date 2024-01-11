adb8bb12959d771c65a21cf2847a9850da03a358
nblack0917 <nickablack@gmail.com>
2024-01-11 15:25:31 -0600

    added distance and pricing

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -6,10 +6,12 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Icon } from "@rneui/themed";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectTravelTimeInformation } from "../slices/navSlice";
 
 const data = [
   {
@@ -32,8 +34,12 @@ const data = [
   },
 ];
 
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
+  const [selected, setSelected] = useState(null);
   const navigation = useNavigation();
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
 
   return (
     <SafeAreaView className="-mt-10 bg-white flex-grow">
@@ -44,23 +50,51 @@ const RideOptionsCard = () => {
         >
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
-        <Text className="text-center py-5 text-xl">Select a Ride</Text>
+        <Text className="text-center py-5 text-xl">
+          Select a Ride - {travelTimeInformation?.distance.text}
+        </Text>
       </View>
       <FlatList
         const
         data={data}
         keyExtractor={(item) => item.id}
-        renderItem={({ item: { id, title, multiplier, image } }) => (
-          <TouchableOpacity className="flex-row justify-between items-center px-10">
+        renderItem={({ item: { id, title, multiplier, image }, item }) => (
+          <TouchableOpacity
+            onPress={() => setSelected(item)}
+            className={`flex-row justify-between items-center px-10 ${
+              id === selected?.id && "bg-gray-200"
+            }`}
+          >
             <Image style={styles.itemContainer} source={{ uri: image }} />
             <View className="-ml-6">
               <Text className="text-xl font-semibold">{title}</Text>
-              <Text>Travel time...</Text>
+              <Text>{travelTimeInformation?.duration?.text}</Text>
             </View>
-            <Text className="text-xl">$99</Text>
+            <Text className="text-xl">
+              {new Intl.NumberFormat("en-us", {
+                style: "currency",
+                currency: "USD",
+              }).format(
+                (travelTimeInformation?.duration.value *
+                  SURGE_CHARGE_RATE *
+                  multiplier) /
+                  100
+              )}
+            </Text>
           </TouchableOpacity>
         )}
       />
+
+      <View className="mt-auto border-t border-gray-200">
+        <TouchableOpacity
+          disabled={!selected}
+          className={`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
+        >
+          <Text className="text-center text-white text-xl">
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,15 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Platform,
+} from "react-native";
 import React from "react";
 import Map from "../components/Map";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import NavigateCard from "../components/NavigateCard";
 import RideOptionsCard from "../components/RideOptionsCard";
+import { Icon } from "@rneui/themed";
+import { useNavigation } from "@react-navigation/native";
 
 const MapScreen = () => {
   const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
 
   return (
     <View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("HomeScreen")}
+        className="absolute top-16 left-8 bg-gray-100 z-50 p-3 rounded-full shadow-lg"
+        style={Platform.OS === "android" && styles.shadow}
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
+
       <View className="h-1/2">
         <Map />
       </View>
@@ -44,4 +61,16 @@ const MapScreen = () => {
 
 export default MapScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+
+    elevation: 6,
+  },
+});
